4b968bc8072654f05fb26fc361f46d174d6479f9
Di Xu <xudifsd@gmail.com>
2018-05-31 17:09:29 +0800

    handle login

--- src/web.py
+++ src/web.py
@@ -9,6 +9,10 @@ import Cookie
 import BaseHTTPServer
 import urlparse
 import shutil
+import threading
+import logging
+
+log = logging.getLogger("web")
 
 import chess
 
@@ -27,7 +31,8 @@ MIME_MAPPING = {
 
 
 class HTTPRequest(object):
-    def __init__(self, context, path, groups, cookie, args, data, method):
+    def __init__(self, headers, context, path, groups, cookie, args, data, method):
+        self.headers = headers
         self.context = context
         self.path = path
         self.groups = groups
@@ -38,29 +43,48 @@ class HTTPRequest(object):
 
 
 class HTTPResponse(object):
-    def __init__(self, status, headers, body):
+    def __init__(self, status, headers, body, cookie):
         self.status = status
         self.headers = headers
         self.body = body
+        self.cookie = cookie
+
+
+def redirect(url, cookie=None, status=307):
+    headers = {"Content-Type": "text/html; charset=utf-8",
+                "Location": url}
+    return HTTPResponse(status, headers, None, cookie)
 
 
 def handle_home_page(req):
-    headers = {"Content-Type": "text/html; charset=utf-8",
-                "Location": "/room/123"}
-    return HTTPResponse(307, headers, None)
+    return redirect("/static/home.html")
+
+
+def handle_login(req):
+    if req.args.get("name") is None:
+        return redirect("/static/home.html")
+
+    new_id = req.context.uid_handler.gen_new_id(req.args.get("name"))
+    req.cookie["uid"] = str(new_id)
+    return redirect("/room/123", cookie=req.cookie, status=303)
 
 
 def handle_room(req):
+    user_name = req.context.uid_handler.get_user_name(req.cookie)
+    if user_name is None:
+        return redirect("/")
+
     room_number = int(req.groups[0])
     headers = {"Content-Type": "text/html; charset=utf-8"}
+
     return HTTPResponse(200, headers,
-            "<html><body>welcom to room %d, your args is %s, your cookie is %s</body></html>" % \
-                    (room_number, str(req.args), req.cookie.output()))
+            "<html><body>%s, welcom to room %d</body></html>" % \
+                    (user_name, room_number), req.cookie)
 
 
 def handle_404(req):
     header = {"Content-Type": "text/plain; charset=utf-8"}
-    return HTTPResponse(404, header, "nothing here, you'r using %d" % (req.method))
+    return HTTPResponse(404, header, "nothing here, you'r using %d" % (req.method), req.cookie)
 
 
 def handle_static(req):
@@ -75,7 +99,7 @@ def handle_static(req):
     else:
         header = {"Content-Type": MIME_MAPPING[parts[-1]]}
 
-    return HTTPResponse(200, header, open(path))
+    return HTTPResponse(200, header, open(path), req.cookie)
 
 
 def is_match_all(pattern, string):
@@ -98,24 +122,30 @@ def route(route_info, method, path):
 
 
 class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    def is_file(self, obj):
+    def _is_file(self, obj):
         fn_type = type(self.__init__)
         if hasattr(obj, "read") and hasattr(obj, "close"):
             return True
         return False
 
-    def _set_return(self, status, headers, body):
-        self.send_response(status)
+    def _set_return(self, resp):
+        body = resp.body
 
-        for k, v in headers.items():
+        self.send_response(resp.status)
+
+        for k, v in resp.headers.items():
             self.send_header(k, v)
+
+        if resp.cookie is not None:
+            for k, v in resp.cookie.items():
+                self.send_header("Set-Cookie", "%s=%s" % (str(k), str(v.coded_value)))
 
         self.end_headers()
 
         if body is not None:
             if isinstance(body, str):
                 self.wfile.write(body)
-            elif self.is_file(body):
+            elif self._is_file(body):
                 try:
                     shutil.copyfileobj(body, self.wfile)
                 finally:
@@ -128,14 +158,8 @@ class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
         fn, groups = route(context.route_info, method, path)
         if fn is None:
-            self._set_return(404, {"Content-Type": "text/plain; charset=utf-8"}, "nothing here")
+            self._set_return(HTTPResponse(404, {"Content-Type": "text/plain; charset=utf-8"}, "nothing here"), None)
             return
-
-        args = {}
-        for k, v in urlparse.parse_qsl(parse_result.params):
-            args[k] = v
-        for k, v in urlparse.parse_qsl(parse_result.query):
-            args[k] = v
 
         if self.headers.get("Content-Length") is not None:
             length = int(self.headers["Content-Length"])
@@ -143,17 +167,25 @@ class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         else:
             data = ""
 
+        args = {}
+        for k, v in urlparse.parse_qsl(parse_result.params):
+            args[k] = v
+        for k, v in urlparse.parse_qsl(parse_result.query):
+            args[k] = v
+
+        if self.headers.get("Content-Type") == "application/x-www-form-urlencoded":
+            for k, v in urlparse.parse_qsl(data):
+                args[k] = v
+
         cookie_data = self.headers.get("Cookie")
         if cookie_data is not None:
             cookie = Cookie.SimpleCookie(cookie_data)
         else:
             cookie = Cookie.SimpleCookie()
 
-        request = HTTPRequest(context, path, groups, cookie, args, data, method)
+        request = HTTPRequest(self.headers, context, path, groups, cookie, args, data, method)
 
-        resp = fn(request)
-
-        self._set_return(resp.status, resp.headers, resp.body)
+        self._set_return(fn(request))
 
     def do_GET(self):
         self.process(HTTPServer.GET)
@@ -177,13 +209,35 @@ class HTTPHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.process(HTTPServer.OPTIONS)
 
 
+class UidHandler(object):
+    def __init__(self):
+        self.lock = threading.RLock()
+        self.array = []
+
+    def get_user_name(self, cookie):
+        if cookie.get("uid") is None or not cookie.get("uid").value.isdigit():
+            return None
+
+        id = int(cookie.get("uid").value)
+
+        with self.lock:
+            if id < 0 or id >= len(self.array):
+                return None
+            return self.array[id]
+
+    def gen_new_id(self, name):
+        with self.lock:
+            self.array.append(name)
+            return len(self.array) - 1
+
+
 class HTTPContext(object):
     """ member of this instance should be thread safe """
-    def __init__(self, route_info, chess_session_class, rooms_info, uid_counter):
+    def __init__(self, route_info, chess_session_class, rooms_info, uid_handler):
         self.route_info = route_info
         self.chess_session_class = chess_session_class
         self.rooms_info = rooms_info
-        self.uid_counter = uid_counter
+        self.uid_handler = uid_handler
 
 
 class HTTPServer(BaseHTTPServer.HTTPServer):
@@ -193,13 +247,14 @@ class HTTPServer(BaseHTTPServer.HTTPServer):
         BaseHTTPServer.HTTPServer.__init__(self, *args, **kw)
         route_info = (
                 (HTTPServer.GET, re.compile("/"), handle_home_page),
+                (HTTPServer.POST, re.compile("/"), handle_login),
                 (HTTPServer.GET, re.compile("/room/([0-9]+)"), handle_room),
                 (HTTPServer.GET, re.compile("/static/(.*)"), handle_static),
                 (HTTPServer.ANY, re.compile(".*"), handle_404)
                 )
 
         # TODO implement cookie and session handler
-        self.context = HTTPContext(route_info, chess.Session, None, None)
+        self.context = HTTPContext(route_info, chess.Session, None, UidHandler())
 
 
 def run(server_class=HTTPServer, handler_class=HTTPHandler, port=8080):
@@ -210,6 +265,8 @@ def run(server_class=HTTPServer, handler_class=HTTPHandler, port=8080):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+            level=logging.INFO)
     if len(sys.argv) == 2:
         run(port=int(sys.argv[1]))
     else:
